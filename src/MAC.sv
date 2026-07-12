907232184c4c39edca5948395e0f5d9102067a3c
jtkim00000 <jessetkim00000@gmail.com>
2026-07-11 22:02:08 -0500

    mac submodule complete, needs verification

--- src/MAC.sv
+++ src/MAC.sv
@@ -1,13 +1,17 @@
 module mac (
-    input wire signed [7:0] activation,
-    input wire signed [7:0] weight,
-    input wire signed [17:0] in_sum,
-    output signed [17:0] out_sum
+    input logic signed [7:0] activation,
+    input logic signed [7:0] weight,
+    input logic signed [31:0] in_sum,
+    output logic signed [31:0] out_sum
 );
-    wire signed [17:0] product;
+    logic signed [31:0] product;
 
-    assign product = activation * weight;
+    always_comb begin
 
-    assign out_sum = in_sum + product;
+        product = activation * weight;
+
+        out_sum = in_sum + product;
+        
+    end
 
 endmodule
