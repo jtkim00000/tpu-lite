44563ffe0078a906f052700b06887159707e1bc2
jtkim00000 <jessetkim00000@gmail.com>
2026-07-14 02:31:28 -0500

    added mac interface

--- src/MAC.sv
+++ src/MAC.sv
@@ -1,17 +1,17 @@
-module mac (
-    input logic signed [7:0] activation,
-    input logic signed [7:0] weight,
-    input logic signed [31:0] in_sum,
-    output logic signed [31:0] out_sum
-);
+interface mac_intf();
+    input logic signed [7:0] activation, weight;
+    input logic signed [31:0] in_sum;
+    output logic signed [31:0] out_sum;
+endinterface: mac_intf
+
+module mac (mac_intf i);
     logic signed [31:0] product;
 
     always_comb begin
 
-        product = activation * weight;
+        product = intf.activation * intf.weight;
+        out_sum = intf.in_sum + product;
 
-        out_sum = in_sum + product;
-        
     end
 
-endmodule
+endmodule: mac
